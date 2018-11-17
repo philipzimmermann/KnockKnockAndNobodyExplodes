1c12b2afb70b8b974ec51054b12010b8c8346a11
stiba99 <45126917+stiba99@users.noreply.github.com>
2018-11-18 00:54:42 +0100

    Add files via upload

--- Clock/Clock.ino
+++ Clock/Clock.ino
@@ -1,42 +1,78 @@
-int Eingang = 6;
-int Ausgabe = 3;
-int Zeit = 600;
+int Start = 13;
+int Eingang = 2;
+int Ausgabe = 0;
+int Zeit = 300;
+int stupid = 10;
+bool started = false;
+int n = 1;
 #include <Display.h>
 #include <Digit.h>
 #include <Segment.h>
 
-const byte digit_pins[4] {6, 9, 10, 0}; // From com1 - com4
-const byte segment_pins[7] {7, 11, 2, 4, 5, 8, 1}; // From a - g (abc...g)
-const byte dp_pin = 3;
-char* timeOut = "00.00";
+const byte digit_pins[4] {1, A0, 3, 4}; // From com1 - com4
+const byte segment_pins[7] {12, 10, 8, 7, 6, 11, 9}; // From a - g (abc...g)
+const byte dp_pin = 5;
+char* timeOut = "05.00";
 
 Display d(digit_pins, segment_pins, dp_pin);
 
 void setup() {
-  for (int i = 0; i <= 11; i++){
+  for (int i = 0; i <= 13; i++) {
     pinMode(i, OUTPUT);
   }
-  pinMode(Eingang,INPUT);
-  pinMode(Ausgabe,OUTPUT);
-  //attachInterrupt(digitalPinToInterrupt(Eingang), Strike, RISING);
+  pinMode(A0,OUTPUT);
+  pinMode(Start, INPUT_PULLUP);
+  pinMode(Ausgabe, OUTPUT);
+  pinMode(Eingang, INPUT_PULLUP);
+  //attachInterrupt(Eingang, Incomeing, FALLING);
+  Serial.begin(9800);
 }
 
 void loop() {
-  /*if (Zeit == 0) {
-    Ausgabe = 6;
+  Serial.print("Test");
+  if(!started) {
+    if (!digitalRead(Start)) {
+    started = true;
+    digitalWrite(Ausgabe,HIGH);
+    delay(10);
+    digitalWrite(Ausgabe,LOW);
+    Serial.println("LOS");
+    }
+  } else 
+  { 
+  if (Zeit == -1) {
+    digitalWrite(Ausgabe, HIGH);
+    Serial.println("ENDE");
+    //d.print("88.88");
+    started = false;
+    Zeit = 300;
   } else {
-  Zeit--;
+
+    if (n == stupid) {
+      int m = Zeit / 60;
+      int s = Zeit % 60;
+      timeOut[0] = m / 10 + 48;
+      timeOut[1] = m % 10 + 48;
+      timeOut[3] = s / 10 + 48;
+      timeOut[4] = s % 10 + 48;
+      //Serial.println(timeOut);
+      Zeit--;
+      n = 1;
+    }
+    n++;
   }
-  printTime(Zeit/60,Zeit%60); //TODO void printTime(int m,int s);
-  */
-  d.print("13.37");
-}
 
-void printTime(int m, int s) {
+  d.print(timeOut);
+  delay(10);
+  }
+  }
   
-}
-
 void Strike()
 {
-  Zeit = Zeit - 200;
+  stupid -= 15;
+}
+
+void Incomeing()
+{
+  Zeit = -1; 
 }
