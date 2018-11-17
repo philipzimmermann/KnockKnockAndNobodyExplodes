88e8e74c96cfb73be16ac4d65fe1fb116fba6d0b
Philip Z <philip.zimmermann@tum.de>
2018-11-17 20:04:23 +0100

    aodfneipafn

--- KlatschLED/KlatschLED.ino
+++ KlatschLED/KlatschLED.ino
@@ -2,11 +2,13 @@
 //2016.06.13
 
 int Led = 13; //define LED port
-int buttonpin = 3; //define switch port
+const int buttonpin = 2; //define switch port
 int  val; //define digital variable val
 unsigned long lastSoundDetectTime = 0;
 boolean ledZustand = false;
 int counter = 0;
+const int tolerance = 600; //This value is tolerated +- for the time between knocks
+const int waitingDelay = 2;
 
 void  setup()
 {
@@ -14,42 +16,43 @@ void  setup()
   pinMode(buttonpin, INPUT); //define switch as a output port
   Serial.begin(9600);
 }
+
 void  loop()
 {
-
+  Serial.println(checkKnockPattern(2000,3));
 }
 
 boolean checkKnockPattern(int time_dist, int knock_number) {
   int knockCounter = 0;
-  val = digitalRead(buttonpin); //read the value of the digital interface 3 assigned to val
-  
-  if (val == HIGH) //when the switch sensor have signal, LED blink
-  {
+  //wait for first knock
+  Serial.println("Vor schleife");
+  do {
+    delay(waitingDelay);
+    val = digitalRead(buttonpin); //read the value of the digital interface 3 assigned to val
+  } while (val == LOW);
+    Serial.println("Nach schleife");
+
+
+  while (knockCounter < knock_number) {
     knockCounter++;
-    lastSoundDetectTime = millis();
-    Serial.println("TON 1 WAHRGENOMMEN!");
-    delay(80);
-    val = digitalRead(buttonpin);
-    if (val == LOW) {
-      val = digitalRead(buttonpin);
-      Serial.println("Warte auf Ton 2");
-      boolean ton2 = false;
-      while (((millis() - lastSoundDetectTime) < 800) && (val == LOW)) {
-        val = digitalRead(buttonpin);
-        if (val == HIGH)
-        {
-          ledZustand = !ledZustand;
-          digitalWrite(Led, ledZustand);
-          Serial.println("Ton 2 wahrgenommen");
-          ton2 = true;
-          delay(30);
-        }
-      }
-      if (!ton2) {
-        Serial.println("kein Ton 2 wahrgenommen!");
-      }
-      ton2 = false;
+    Serial.print("TON ");
+    Serial.print(knockCounter);
+    Serial.println("WAHRGENOMMEN!");
+    
+    if (knockCounter == knock_number){
+      //Erfolgreich
+      Serial.println("checkKnockPattern erfolgreich!!!");
+      return true;
     }
+    lastSoundDetectTime = millis();
+    do {
+      delay(waitingDelay);
+      val = digitalRead(buttonpin); //read the value of the digital interface 3 assigned to val
+    } while (val == LOW);
+    long newSoundDetectTime = millis();
+    long timeDiff = newSoundDetectTime-lastSoundDetectTime;
+    if(timeDiff > time_dist + tolerance) strike();
+    if(timeDiff < time_dist - tolerance) strike();
   }
 }
 
@@ -82,5 +85,9 @@ void doubleKnock() {
       ton2 = false;
     }
   }
+}
+
+void strike() {
+  //TODO
 }
 
